ea149fcb50b3d92dd906bd41866073ea8db8306f
Nawaf Abdullah <nawaf97k@gmail.com>
2017-10-09 17:37:31 -0400

    Added a function that accounts for floor

--- ClassicalMechanics/ProjectTrajectory.py
+++ ClassicalMechanics/ProjectTrajectory.py
@@ -3,32 +3,43 @@ import math
 
 class Projectile:
 
+    gravity = 9.81
+
     def TrajectoryNoFloor(self, time, vo, azimuth, elevation, xo, yo, zo):
         print('Time (s)        x-location (m)        y-location        z-location')
         for t in range(time+1):
             x = vo * math.cos(math.radians(azimuth)) * math.cos(math.radians(elevation)) * t + xo
             y = vo * math.sin(math.radians(azimuth)) * math.cos(math.radians(elevation)) * t + yo
-            z = vo * math.sin(math.radians(elevation)) * t - 0.5 * 9.81 * t * t + zo
+            z = vo * math.sin(math.radians(elevation)) * t - 0.5 * Projectile.gravity * t * t + zo
             print(t, end='')
             print("               %.2f" %x, end='')
             print("                  %.2f" %y, end='')
             print("                %.2f" %z)
+        print("_________________________________________________________")
 
     def TrajectoryFloor(self, time, vo, azimuth, elevation, xo, yo, zo):
         print('Time (s)        x-location (m)        y-location        z-location')
-        for t in range(time):
+        for t in range(time+1):
             x = vo * math.cos(math.radians(azimuth)) * math.cos(math.radians(elevation)) * t + xo
             y = vo * math.sin(math.radians(azimuth)) * math.cos(math.radians(elevation)) * t + yo
-            z = vo * math.sin(math.radians(elevation)) * t - 0.5 * 9.81 * t * t + zo
-            if z <= 0:
-                xf = x
-                yf = y
-                zf = z
+            z = vo * math.sin(math.radians(elevation)) * t - 0.5 * Projectile.gravity * t * t + zo
+            print(t, end='')
+            print("               %.2f" %x, end='')
+            print("                  %.2f" %y, end='')
+            print("                %.2f" %z)
+            if z < 0:
+                break
+        print("________________________Floor_______________________________")
 
 
 n_projectile = int(input('Number of projectiles > '))
 projectile_list= [Projectile for i in range(n_projectile)]
 ti = int(input('Time interval > '))
+g = input('Enable earth gravity? (y/n) > ')
+if g=='n':
+    Projectile.gravity = 0
+floor_switch = input('Put floor? (y/n) > ')
+
 
 for j in range(n_projectile):
     print('// Parameters for object number [', j+1, '] // ')
@@ -38,6 +49,11 @@ for j in range(n_projectile):
     xi = float(input('launch x-coordinates > '))
     yi = float(input('launch y-coordinates > '))
     zi = float(input('launch z-coordinates > '))
-    projectile_list[j].TrajectoryNoFloor(projectile_list[j], ti, vi, az, el, xi, yi, zi)
+    if floor_switch == 'n':
+        projectile_list[j].TrajectoryNoFloor(projectile_list[j], ti, vi, az, el, xi, yi, zi)
+    else:
+        projectile_list[j].TrajectoryFloor(projectile_list[j], ti, vi, az, el, xi, yi, zi)
+
+
 
 
