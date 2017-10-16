59d3ff18b8f9ba4f44f2d228abcec849e9938a5b
Nawaf Abdullah <nawaf97k@gmail.com>
2017-10-16 14:28:27 -0400

    Fixed last time point
    Fixed the table to show the correct values of right as the object hit the ground.

--- ClassicalMechanics/KineticPotentialEnergy.py
+++ ClassicalMechanics/KineticPotentialEnergy.py
@@ -5,28 +5,25 @@ from prettytable import PrettyTable
 
 class KineticPotentialEnergy:
 
-    def OutputTable(self):
+    def ConversionTable(self):
+        m  = float(input("Object's mass > "))
+        hi = float(input("Object's initial height > "))
         table = PrettyTable(['Time (s)', 'Height(m)', 'Potential Energy(J)', 'Velocity(m/s)', 'Kinetic Energy(J)'])
-        m = float(input('Mass of the object (kg) > '))
-        hi = float(input('Distance of the object from the ground in meters > '))
-        #final values for when the object finally hit the ground - START
-        tf = math.sqrt(2*hi/9.81)
-        hf = Distance.DistanceFinal2Return(Distance(), 0, 0, tf, 9.81)
-        dh = hi - hf
-        PE_f = m * 9.81 * dh
-        vf = Velocity.VelocityFinal1Return(Velocity(), 0, 9.81, tf)
-        KE_f = 0.5 * m * vf * vf
-        # ~~~~~~END
-        for t in range(0, 9):
-            height = hi - Distance.DistanceFinal2Return(Distance(), 0, 0, t, 9.81)
-            if height <= 0:
-                table.add_row(["%.2f" %tf, "%.2f" %dh, "%.2f" %PE_f, "%.2f" %vf, "%.2f" %KE_f])
+        for t in range(0, 15):
+            h  = hi - Distance.DistanceFinal2Return(Distance(), 0, 0, t, 9.81)
+            if h <= 0:
+                tf = math.sqrt(2*hi/9.81)
+                hf = hi - Distance.DistanceFinal2Return(Distance(), 0, 0, tf, 9.81)
+                pe = m * 9.81 * hf
+                vf  = Velocity.VelocityFinal1Return(Velocity(), 0, 9.81, tf)
+                ke = 0.5 * m * vf * vf
+                table.add_row(["%.2f" % tf, "%.2f" % hf, "%.2f" % pe, "%.2f" % vf, "%.2f" % ke])
                 break
-            PE = mass * 9.81* height
-            v = Velocity.VelocityFinal1Return(Velocity(), 0, 9.81, t)
-            KE = 0.5 * mass * v * v
-            table.add_row([t, "%.2f" %height, "%.2f" %PE, "%.2f" %v, "%.2f" %KE])
+            pe = m * 9.81 * h
+            v  = Velocity.VelocityFinal1Return(Velocity(), 0, 9.81, t)
+            ke = 0.5 * m * v * v
+            table.add_row(["%.2f" % t, "%.2f" % h, "%.2f" % pe, "%.2f" % v, "%.2f" % ke])
         print(table)
 
 
-KineticPotentialEnergy.OutputTable(KineticPotentialEnergy())
+KineticPotentialEnergy.ConversionTable(KineticPotentialEnergy())
