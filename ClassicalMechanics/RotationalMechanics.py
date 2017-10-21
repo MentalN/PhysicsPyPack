89b329a13555657f840c3dac96e72445bafa3fab
Nawaf Abdullah <nawaf97k@gmail.com>
2017-10-21 09:36:09 -0400

    Add files via upload

---
+++ ClassicalMechanics/RotationalMechanics.py
@@ -0,0 +1,69 @@
+import math
+
+
+class AngularVelocity:
+
+    omega = []
+
+    def ang_velocity_position(self):
+        ti = float(input("Initial position angle > "))
+        tf = float(input("Final position angle > "))
+        dt = float(input("Change in time > "))
+        w = (tf - ti)/dt
+        print("Angular velocity w =", "%.2f" % w, "rad/s")
+        store = input("Store this angular velocity instance (y/n)? ")
+        if store == 'y':
+            AngularVelocity.omega.append(v)
+            print("Angular velocity instance stored as number:", len(AngularVelocity.omega))
+        return w
+
+    def ang_velocity_tangential(self):
+        r = float(input("Radius > "))
+        v = float(input("Tangential velocity > "))
+        w = v/r
+        print("Angular velocity w =",  "%.2f" %w, "rad/s")
+        store = input("Store this angular velocity instance (y/n)? ")
+        if store == 'y':
+            AngularVelocity.omega.append(v)
+            print("Angular velocity instance stored as number:", len(AngularVelocity.omega))
+        return w
+
+    def ang_velocity_frequency(self):
+        f = float(input("Frequency of revolutions per second > "))
+        w = 2 * math.pi * f
+        print("Angular velocity w =",  "%.2f" %w, "rad/s")
+        store = input("Store this angular velocity instance (y/n)? ")
+        if store == 'y':
+            AngularVelocity.omega.append(v)
+            print("Angular velocity instance stored as number:", len(AngularVelocity.omega))
+        return w
+
+    def ang_velocity_arc_length(selfs):
+        s  = float(input("Length of arc > "))
+        r  = float(input("radius > "))
+        dt = float(input("Change in time > "))
+        w  = s/r*dt
+        print("Angular velocity w =", "%.2f" % w, "rad/s")
+        store = input("Store this angular velocity instance (y/n)? ")
+        if store == 'y':
+            AngularVelocity.omega.append(v)
+            print("Angular velocity instance stored as number:", len(AngularVelocity.omega))
+        return w
+
+    def ang_velocity_acceleration_time(self):
+        wi = float(input("Initial angular velocity > "))
+        a  = float(input("angular acceleration > "))
+        t  = float(input("Time > "))
+        w  = wi + a*t
+        print("Angular velocity w =", "%.2f" % w, "rad/s")
+        store = input("Store this angular velocity instance (y/n)? ")
+        if store == 'y':
+            AngularVelocity.omega.append(v)
+            print("Angular velocity instance stored as number:", len(AngularVelocity.omega))
+        return w
+
+
+
+
+
+
