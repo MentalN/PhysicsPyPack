b07f072cd4e6960bf7b31e0fac5b281cd7b8d96f
Nawaf Abdullah <nawaf97k@gmail.com>
2017-11-04 22:30:32 -0400

    Final for prototype

--- ClassicalMechanics/Gravity.py
+++ ClassicalMechanics/Gravity.py
@@ -1,33 +1,41 @@
 class Gravity:
+
     G  = 6.67408*10**-11
     Me = 5.972*10**24
     Re = 6.371*10**6
 
-    def GraviationalForce(self, r, m):
-        F = (Gravity.G*Gravity.Me*m)/((r+Gravity.Re)**2)
-        print('Gravitational Force = ', "%.2f" %F, 'N')
+    F_g = []
+    U_g = []
+
+    def gravitational_force(self, r, m):
+        F = (self.G*self.Me*m)/((r+self.Re)**2)
+        print('Gravitational Force = ', "%.2f" % F, 'N')
+        store = input("Store this gravitational force instance? (y/n)")
+        if store == 'y':
+            self.F_g.append(F)
+            print("Instance stored as number [", len(self.F_g), "]")
         return F
 
-    def GravitationalPotential(self, r, m):
-        U = (Gravity.G*Gravity.Me*m)/(r+Gravity.Re)
-        print('Gravitational Potential Energy = ', "%.2f" %U, 'J')
+    def gravitational_potential(self, r, m):
+        U = (self.G*self.Me*m)/(r+self.Re)
+        print('Gravitational Potential Energy = ', "%.2f" % U, 'J')
+        store = input("Store this gravitational potential instance? (y/n)")
+        if store == 'y':
+            self.U_g.append(U)
+            print("Instance stored as number [", len(self.U_g), "]")
         return U
 
+    def gravity_menu(self):
+        print('[1] - Gravitational Force')
+        print('[2] - Gravitational Potential Energy')
+        select = int(input('Select an option > '))
 
+        distance = float(input('Distance from earth surface > '))
+        mass = float(input('Mass of the object > '))
 
-def menu():
-    print('[1] - Graviational Force')
-    print('[2] - Gravitational Potential Energy')
-    select = int(input('Select an option > '))
-
-    distance = float(input('Distance from earth surface > '))
-    mass = float(input('Mass of the object > '))
-
-    if select == 1:
-        Gravity.GraviationalForce(Gravity(), distance, mass)
-    elif select == 2:
-        Gravity.GravitationalPotential(Gravity(), distance, mass)
-    menu()
-
-    
-menu()
+        if select == 1:
+            self.gravitational_force(distance, mass)
+        elif select == 2:
+            self.gravitational_potential(distance, mass)
+        else:
+            print("invalid Entry!")
