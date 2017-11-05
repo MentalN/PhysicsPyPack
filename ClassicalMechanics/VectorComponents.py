6565f882bd4c83b68cc512ee16430d6409515172
Nawaf Abdullah <nawaf97k@gmail.com>
2017-11-05 00:17:30 -0400

    Not final yet but more capable

--- ClassicalMechanics/VectorComponents.py
+++ ClassicalMechanics/VectorComponents.py
@@ -4,44 +4,70 @@
 import math
 
 
-class TwoDimensions:
+class Vector:
+
+    V_comp = []
+    V_mag = []
+    Ang = []
+
+
+class TwoDimensions(Vector):
 
     def components(self):
         magnitude = float(input('Vector magnitude  > '))
         angle     = float(input('Angle from X-axis in degrees > '))
         vx = magnitude * math.cos(math.radians(angle))
         vy = magnitude * math.sin(math.radians(angle))
-        print('X-component =', "%.2f" %vx)
-        print('y-component =', "%.2f" %vy)
+        V = [vx, vy]
+        print("Vector components: ", V)
+        store = input("Store this vector instance? (y/n) ")
+        if store == 'y':
+            self.V_comp.append(V)
+            print("Vector instance stored as number [", len(self.V_comp), "]")
+        return V
 
-    def magnitudeGivenComponents(self):
+    def magnitude_given_components(self):
         vx = float(input('x - component > '))
         vy = float(input('y - component > '))
         v  = math.sqrt(vx*vx + vy*vy)
-        print('Magnitude =', "%.2f" %v)
+        print('Vector magnitude =', "%.2f" % v)
+        store = input("Store this vector magnitude instance? (y/n) ")
+        if store == 'y':
+            self.V_mag.append(v)
+            print("Vector magnitude instance stored as number [", len(self.V_mag), "]")
+        return v
 
-    def magnitudeGivenAngle(self):
+    def magnitude_given_angle(self):
         vc    = float(input('Component value > '))
         dim   = input('[x], [y]? Given component direction > ')
-        angle = float(input('Component angle > '))
+        angle = float(input('Component angle in degrees > '))
         if dim == 'x':
             v = vc / math.cos(math.radians(angle))
         elif dim == 'y':
             v = vc / math.sin(math.radians(angle))
         else:
-            print('Direction input error!')
-            TwoDimensions.magnitudeGivenAngle(TwoDimensions())
-        print('Vector magnitude = ', "%.2f" %v)
+            print('Angle input error!')
+            self.magnitude_given_angle()
+        print('Vector magnitude = ', "%.2f" % v)
+        store = input("Store this vector magnitude instance? (y/n) ")
+        if store == 'y':
+            self.V_mag.append(v)
+            print("Vector magnitude instance stored as number [", len(self.V_mag), "]")
+        return v
 
-    def angleBetween(self):
+    def angle_between_components(self):
         vx = float(input("Vector x-component > "))
         vy = float(input("Vector y-component > "))
         theta = math.degrees(math.atan(vy / vx))
         print("Angle between x-component and y-component: ", "%.2f" % theta, "Degrees")
+        store = input("Store this angle instance? (y/n) ")
+        if store == 'y':
+            self.Ang.append(theta)
+            print("Angle magnitude instance stored as number [", len(self.Ang), "]")
         return theta
 
 
-class ThreeDimensions:
+class ThreeDimensions(Vector):
 
     def components(self):
         magnitude   = float(input('Vector magnitude  > '))
@@ -50,19 +76,27 @@ class ThreeDimensions:
         vx = magnitude * math.cos(math.radians(azimuth)) * math.cos(math.radians(elevation))
         vy = magnitude * math.sin(math.radians(azimuth)) * math.cos(math.radians(elevation))
         vz = magnitude * math.sin(math.radians(elevation))
-        print('X-component =', "%.2f" %vx)
-        print('y-component =', "%.2f" %vy)
-        print('z-component =', "%.2f" %vz)
+        V = [vx, vy, vz]
+        print("Vector components: ", V)
+        store = input("Store this vector instance? (y/n) ")
+        if store == 'y':
+            self.V_comp.append(V)
+            print("Vector instance stored as number [", len(self.V_comp), "]")
+        return V
 
-    def magnitudeGivenComponents(self):
+    def magnitude_given_components(self):
         vx = float(input('x - component > '))
         vy = float(input('y - component > '))
         vz = float(input('z - component > '))
         v  = math.sqrt(vx*vx + vy*vy + vz*vz)
-        print('Magnitude =', "%.2f" %v)
+        print('Magnitude =', "%.2f" % v)
+        store = input("Store this vector magnitude instance? (y/n) ")
+        if store == 'y':
+            self.V_mag.append(v)
+            print("Vector magnitude instance stored as number [", len(self.V_mag), "]")
         return v
 
-    def magnitudeGivenAngle(self):
+    def magnitude_given_angle(self):
         vc    = float(input('Component value > '))
         dim   = input('x, y, or z? Given component direction > ')
         elevation = float(input('Component elevation angle > '))
@@ -75,20 +109,24 @@ class ThreeDimensions:
         elif dim == 'z':
             v = vc / math.sin(math.radians(elevation))
         else:
-            print('Direction input error!')
-            ThreeDimensions.magnitudeGivenAngle(ThreeDimensions())
-        print('Vector magnitude = ', "%.2f" %v)
+            print('Angle input error!')
+            self.magnitude_given_angle(ThreeDimensions())
+        print('Vector magnitude = ', "%.2f" % v)
+        store = input("Store this vector magnitude instance? (y/n) ")
+        if store == 'y':
+            self.V_mag.append(v)
+            print("Vector magnitude instance stored as number [", len(self.V_mag), "]")
         return v
 
 
 twod_commands = {"components": TwoDimensions.components,
-                 "magnitude given components": TwoDimensions.magnitudeGivenComponents,
-                 "magnitude given angle": TwoDimensions.magnitudeGivenAngle,
-                 "angle between components": TwoDimensions.angleBetween}
+                 "magnitude given components": TwoDimensions.magnitude_given_components,
+                 "magnitude given angle": TwoDimensions.magnitude_given_angle,
+                 "angle between components": TwoDimensions.angle_between_components}
 
 threed_commands = {"components": ThreeDimensions.components,
-                   "magnitude given components": ThreeDimensions.magnitudeGivenComponents,
-                   "magnitude given angle": ThreeDimensions.magnitudeGivenAngle}
+                   "magnitude given components": ThreeDimensions.magnitude_given_components,
+                   "magnitude given angle": ThreeDimensions.magnitude_given_angle}
 
 
 def help():
@@ -125,5 +163,3 @@ def menu():
         menu()
     menu()
 
-
-menu()
