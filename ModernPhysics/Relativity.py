dda7d6c7cd854c953f869295804c117bcaa6ce11
Nawaf Abdullah <nawaf97k@gmail.com>
2017-11-06 01:27:32 -0500

    Added length contraction

--- ModernPhysics/Relativity.py
+++ ModernPhysics/Relativity.py
@@ -1,17 +1,18 @@
 import math
 
 
-class Effects:
+class Relativistic:
     c = 299792458
 
     time = []
     mass = []
+    length = []
 
     def time_dilation(self):
         to = float(input("time > "))
         u  = float(input("velocity > "))
         t  = to * (1/math.sqrt(1-(u/self.c)**2))
-        print("dilated time t =", t, "s")
+        print("Dilated time t =", t, "s")
         store = input("Store this time instance? (y/n) ")
         if store == 'y':
             self.time.append(t)
@@ -29,5 +30,15 @@ class Effects:
             print("Instance stored as number [", len(self.mass), "]")
         return m
 
+    def length_contraction(self):
+        Lo = float(input("Proper length > "))
+        u  = float(input("Velocity > "))
+        L  = Lo * math.sqrt(1-(u/self.c)**2)
+        print("Lenght after contraction =", L)
+        store = input("Store this length instance? (y/n) ")
+        if store == 'y':
+            self.mass.append(L)
+            print("Instance stored as number [", len(self.length), "]")
+        return L
 
 
