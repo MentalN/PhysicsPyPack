7e1567fe3b9a1e367e1682d5feb17417f7c482e7
Nawaf Abdullah <nawaf97k@gmail.com>
2017-11-09 13:10:15 -0500

    Add files via upload

---
+++ ModernPhysics/RelativisticDynamics.py
@@ -0,0 +1,9 @@
+class Momentum:
+
+    particles = {"proton": 1.672722 * 10**-27,
+                 "electron": 9.109384 * 10**-31,
+                 "neutron": 1.674929 * 10**-27,
+                 "amu": 1.660540 * 10**-27}
+
+
+
