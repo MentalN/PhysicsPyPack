9b15cd8d6c3032c1b73ade2319ca16c398b23c31
Nawaf Abdullah <nawaf97k@gmail.com>
2017-11-11 14:27:28 -0500

    Update Electrostatic.py

--- ElectricityMagnetism/Electrostatic.py
+++ ElectricityMagnetism/Electrostatic.py
@@ -1,12 +1,10 @@
-import math
+class Coulomb:
 
-
-class Force:
-
-    k = 8.99 * 10 ** -9
+    k = 8.99 * 10 ** 9
     F = []
+    U = []
 
-    def electrostatic_force_two_particles(self):
+    def coulomb_electrostatic_force(self):
         q_1 = float(input("Charge of the first particle > "))
         q_2 = float(input("Charge of the second particles > "))
         r   = float(input("Distance between > "))
@@ -17,3 +15,37 @@ class Force:
             self.F.append(F)
             print("Instance stored as number [", len(self.F), "]")
         return F
+
+    def electric_potential_energy_two_charges(self):
+        q_1 = float(input("Charge of the first particle > "))
+        q_2 = float(input("Charge of the second particles > "))
+        r   = float(input("Distance between > "))
+        U   = self.k*q_1*q_2/r
+        print("Electric potential energy between two particles U =", U)
+        select = input("Store this potential energy instance? (y/n) ")
+        if select == 'y':
+            self.U.append(U)
+            print("Instance stored as number [", len(self.U), "]")
+        return U
+
+    def electric_potential_energy_n_charges(self):
+        n = int(input("Number of charges > "))
+        qi = float(input("Charge of the initial particle > "))
+        q_sum = 0
+        for x in range(n):
+            print("Charge of particle [", x+1, "]")
+            q = float(input("> "))
+            print("Distance from the initial charge > ")
+            r = float(input("> "))
+            q_sum += q/r
+        U = self.k * qi * q_sum
+        print("Electric potential energy between two particles U =", U)
+        select = input("Store this potential energy instance? (y/n) ")
+        if select == 'y':
+            self.U.append(U)
+            print("Instance stored as number [", len(self.U), "]")
+        return U
+
+
+
+
