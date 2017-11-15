fcab27ce0321d2b740dc71318a4fee633a15c0c8
Nawaf Abdullah <nawaf97k@gmail.com>
2017-11-14 22:54:49 -0500

    Final for prototype

--- ModernPhysics/LorentzTransformation.py
+++ ModernPhysics/LorentzTransformation.py
@@ -7,6 +7,7 @@ class Location:
     xl = 0
     yl = 0
     zl = 0
+    tl = 0
     coords = []
 
     def __init__(self):
@@ -21,13 +22,23 @@ class Location:
         r = 1/math.sqrt(1-b**2)
         dim = input("Frame moving in axis (x/y/z) > ")
         if dim == 'x':
+            self.tl = r * (t - u*self.xl/self.c**2)
             self.xl = (self.xl - u*t)*r
         elif dim == 'y':
+            self.tl = r * (t - u*self.yl/self.c**2)
             self.yl = (self.yl - u*t)*r
         elif dim == 'z':
+            self.tl = r * (t - u*self.zl/self.c**2)
             self.zl = (self.zl - u*t)*r
-        
-        
+        else:
+            print("Invalid entry!")
+            self.transform_coordinate()
+        new_coords = [self.xl, self.yl, self.zl, self.tl]
+        print("New transformed coordinates: ", new_coords)
+        store = input("Save this new coordinates instance? (y/n) ")
+        if store == 'y':
+            self.coords.append(new_coords)
+            print("Coordinates saved as instance number: ", len(self.coords))
 
 
 
