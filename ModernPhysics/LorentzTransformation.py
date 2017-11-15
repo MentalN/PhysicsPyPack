6cd2c4fd53fbc6d86ab48969c6158813ccb5d88d
Nawaf Abdullah <nawaf97k@gmail.com>
2017-11-14 22:45:28 -0500

    Update LorentzTransformation.py

--- ModernPhysics/LorentzTransformation.py
+++ ModernPhysics/LorentzTransformation.py
@@ -1,19 +1,35 @@
 import math
-import numpy
 
 
 class Location:
 
     c = 299792458
-
+    xl = 0
+    yl = 0
+    zl = 0
     coords = []
 
     def __init__(self):
-        xl = float(input("x-coordinates > "))
-        yl = float(input("y-coordinates > "))
-        zl = float(input("z-coordinates > "))
+        self.xl = float(input("x-coordinates > "))
+        self.yl = float(input("y-coordinates > "))
+        self.zl = float(input("z-coordinates > "))
 
     def transform_coordinate(self):
+        u = float(input("Velocity of the object in rest frame > "))
+        t = float(input("Time > "))
+        b = u/self.c
+        r = 1/math.sqrt(1-b**2)
+        dim = input("Frame moving in axis (x/y/z) > ")
+        if dim == 'x':
+            self.xl = (self.xl - u*t)*r
+        elif dim == 'y':
+            self.yl = (self.yl - u*t)*r
+        elif dim == 'z':
+            self.zl = (self.zl - u*t)*r
+        
+        
+
+
 
 
 
