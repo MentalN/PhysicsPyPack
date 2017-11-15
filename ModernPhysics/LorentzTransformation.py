084b12c3efbce63d03a2a26dd23c614752603ccd
Nawaf Abdullah <nawaf97k@gmail.com>
2017-11-14 21:40:51 -0500

    Add files via upload

---
+++ ModernPhysics/LorentzTransformation.py
@@ -0,0 +1,21 @@
+import math
+import numpy
+
+
+class Location:
+
+    c = 299792458
+
+    coords = []
+
+    def __init__(self):
+        xl = float(input("x-coordinates > "))
+        yl = float(input("y-coordinates > "))
+        zl = float(input("z-coordinates > "))
+
+    def transform_coordinate(self):
+
+
+
+
+
