52d9a1822da0c5d2a6b9b0672c89367023e39552
Nawaf Abdullah <nawaf97k@gmail.com>
2017-11-29 02:25:36 -0500

    Added Rydberg constant

--- ModernPhysics/Essentials.py
+++ ModernPhysics/Essentials.py
@@ -4,6 +4,7 @@ class Essentials:
     h = 6.62607004e-34
     h_bar = 1.05457180e-34
     pi = 3.14159265359
+    R = 1.097373e7
 
     particles = {"proton": 1.672722 * 10**-27,
                  "electron": 9.109384 * 10**-31,
